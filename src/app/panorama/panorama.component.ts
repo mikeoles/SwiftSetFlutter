bc517aef9be65ae5728466f3e7b595e6eea160c0
Oles <moles@ceiamerica.com>
2018-10-31 15:16:24 -0400

    Add panorama

---
+++ src/app/panorama/panorama.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import panzoom from 'panzoom';
+
+@Component({
+  selector: 'app-panorama',
+  templateUrl: './panorama.component.html',
+  styleUrls: ['./panorama.component.scss']
+})
+export class PanoramaComponent implements OnInit {
+  @Input() outs: Observable<any[]>;
+  @Input() labels: Observable<any[]>;
+  @Input() gridSelectedId: Number;
+  @Input() gridSelectedDisplay: String;
+  @Output() panoramaId = new EventEmitter();
+  @Output() panoramaDisplay = new EventEmitter();
+  currentId: Number;
+  currentDisplay: String;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    console.log('outs: ', this.outs);
+
+    const element = document.getElementById('pano-image');
+    panzoom(element, {
+      maxZoom: 10,
+      minZoom: 1,
+      bounds: false,
+    });
+  }
+
+  ngOnChanges(changes) {
+    //update the current index when the user selects a row from one of the grids
+    if(this.gridSelectedDisplay) this.currentDisplay = this.gridSelectedDisplay;
+    if(this.gridSelectedId!=null) this.currentId = this.gridSelectedId;
+    if(this.currentId==-1) this.currentId = null;
+  }
+
+  annotationClicked(num,display){
+    if(this.currentId!=num){
+      this.currentId=num;
+      this.panoramaId.emit(num);
+    }
+
+    if(this.currentDisplay!==display){
+      this.currentDisplay = display;
+      this.panoramaDisplay.emit(display);
+    }
+  }
+  
+
+
+}
