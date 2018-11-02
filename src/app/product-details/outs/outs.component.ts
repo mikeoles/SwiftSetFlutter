db7915d8db200967755bd38232bbcaace99e779b
Andy Miller <amiller@ceiamerica.com>
2018-11-02 11:00:27 -0400

    Code cleanup

--- src/app/product-details/outs/outs.component.ts
+++ src/app/product-details/outs/outs.component.ts
@@ -1,39 +1,19 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { ApiService } from '../../api.service';
+import { Component, OnInit, EventEmitter, Output, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-outs',
   templateUrl: './outs.component.html',
-  styleUrls: ['./outs.component.scss','../grid-styles.scss']
+  styleUrls: ['./outs.component.scss', '../grid-styles.scss']
 })
 export class OutsComponent implements OnInit {
 
   @Output() outsGridClicked = new EventEmitter();
+  @Input() outs: any[];
   @Input() selectedId: number;
-  selectedRow : number;
-  outs: any[];
-
-  constructor(private apiService: ApiService) {
-    this.apiService.getOuts().subscribe(outs => this.outs = outs);
-  }
 
   ngOnInit() {}
 
-  ngOnChanges(changes) {
-    if(this.selectedId){
-      this.setClickedRow(this.selectedId);
-    }
-  }
-
-  setClickedRow(id){
-    let i: number;
-    this.selectedRow = -1;
-    for(i=0; i<this.outs.length;i++){
-      if(this.outs[i].Id==id){
-        this.selectedRow = i;
-      }
-    }
+  setClickedRow(id) {
     this.outsGridClicked.emit(id);
   }
-
 }
