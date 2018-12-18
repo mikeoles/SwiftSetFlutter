f929e53d7b495d4d46f934b133e17b31dce4f846
Oles <moles@ceiamerica.com>
2018-12-18 11:23:52 -0500

    Product grid keyboard scrolling

--- src/app/product-details/grid/grid.component.ts
+++ src/app/product-details/grid/grid.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, AfterViewChecked, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, EventEmitter, Output, Input, HostListener } from '@angular/core';
 import Label from 'src/app/label.model';
+
+export enum KEY_CODE {
+  UP = 38,
+  DOWN = 40
+}
 
 @Component({
   selector: 'app-grid',
@@ -23,5 +28,30 @@ export class GridComponent implements OnInit, AfterViewChecked {
 
   setClickedRow(id) {
     this.gridClicked.emit(id);
+  }
+
+  // Change selected row on keyup and keydown
+  @HostListener('window:keyup', ['$event'])
+  clickout(event) {
+    if (event.keyCode === KEY_CODE.UP && this.selectedId >= 0) {
+      const index = this.findIndexById(this.selectedId);
+      if (index > 0) {
+        this.gridClicked.emit( this.products[index - 1].id);
+      }
+    } else if (event.keyCode === KEY_CODE.DOWN && this.selectedId >= 0) {
+      const index = this.findIndexById(this.selectedId);
+      if (index < (this.products.length - 1)) {
+        this.gridClicked.emit( this.products[index + 1].id );
+      }
+    }
+  }
+
+  // Return the table index based on a product id
+  findIndexById(id: number) {
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i].id === id) {
+        return i;
+      }
+    }
   }
 }
