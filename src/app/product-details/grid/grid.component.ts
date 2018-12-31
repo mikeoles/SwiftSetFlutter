465cdf286a87632981621ed27da54ed9c4dd3b41
Oles <moles@ceiamerica.com>
2018-12-31 12:31:38 -0500

    Order products by shelf location

--- src/app/product-details/grid/grid.component.ts
+++ src/app/product-details/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked, EventEmitter, Output, Input, HostListener } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, EventEmitter, Output, Input, HostListener, OnChanges, SimpleChanges } from '@angular/core';
 import Label from 'src/app/label.model';
 
 export enum KEY_CODE {
@@ -11,13 +11,65 @@ export enum KEY_CODE {
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent implements OnInit, AfterViewChecked {
+export class GridComponent implements OnInit, AfterViewChecked, OnChanges {
 
   @Output() gridClicked = new EventEmitter();
   @Input() products: Label[];
   @Input() selectedId: number;
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!changes['products'] || this.products.length < 1) {
+      return;
+    }
+    function vertical(a,  b) {
+      if (a.bounds.top < b.bounds.top) {
+        return -1;
+      }
+      if (a.bounds.top > b.bounds.top) {
+        return 1;
+      }
+      return 0;
+    }
+
+    function horizontal(a,  b) {
+      if (a.bounds.left < b.bounds.left) {
+        return -1;
+      }
+      if (a.bounds.left > b.bounds.left) {
+        return 1;
+      }
+      return 0;
+    }
+
+    this.products.sort(vertical);
+
+    let sortedProducts: Label[] = [];
+    let currentProduct = this.products[0];
+    let currentRow: Label[] = [];
+    let nextProduct = currentProduct;
+    currentRow.push(currentProduct);
+
+    for (let i = 1; i < this.products.length - 1; i++) {
+      nextProduct = this.products[i];
+      if (currentProduct.bounds.top + currentProduct.bounds.height < nextProduct.bounds.top) {
+        currentRow.sort(horizontal);
+        sortedProducts = sortedProducts.concat(currentRow);
+        currentRow = [];
+        currentRow.push(nextProduct);
+      } else {
+        currentRow.push(nextProduct);
+      }
+      currentProduct = nextProduct;
+    }
+    currentRow.push(nextProduct);
+    currentRow.sort(horizontal);
+    sortedProducts = sortedProducts.concat(currentRow);
+    this.products = sortedProducts;
+  }
 
   ngAfterViewChecked() {
     const row = document.getElementById(`row-${this.selectedId}`);
