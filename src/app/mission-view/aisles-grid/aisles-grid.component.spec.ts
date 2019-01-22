dedaa250c05d6692e30a65210e875a79b0dd132c
Andy Miller <amcoder@gmail.com>
2019-01-22 16:27:23 -0500

    Add aisle grid tests

--- src/app/mission-view/aisles-grid/aisles-grid.component.spec.ts
+++ src/app/mission-view/aisles-grid/aisles-grid.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AislesGridComponent } from './aisles-grid.component';
+import { Router } from '@angular/router';
 
 describe('AislesGridComponent', () => {
   let component: AislesGridComponent;
@@ -19,10 +20,19 @@ describe('AislesGridComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AislesGridComponent);
     component = fixture.componentInstance;
+    component.missionId = 1;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('navigates to an aisle', () => {
+    const navigateSpy = spyOn(TestBed.get(Router), 'navigate');
+
+    component.viewAisle('2');
+
+    expect(navigateSpy).toHaveBeenCalledWith(['mission/1/aisle/2']);
   });
 });
