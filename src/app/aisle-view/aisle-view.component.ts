b38a12febade3de6b2021b5687d139a8ec16a22c
Oles <moles@ceiamerica.com>
2019-02-22 11:14:53 -0500

    Fix aisle routing

--- src/app/aisle-view/aisle-view.component.ts
+++ src/app/aisle-view/aisle-view.component.ts
@@ -7,6 +7,8 @@ import Label from './../label.model';
 import { ViewEncapsulation } from '@angular/core';
 import { LogoService } from '../logo.service';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute, Params } from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-aisle-view',
@@ -35,7 +37,9 @@ export class AisleViewComponent implements OnInit, OnDestroy {
 
   constructor(private apiService: ApiService,
               private keyboard: KeyboardShortcutsService,
-              private logoService: LogoService) {
+              private logoService: LogoService,
+              private activatedRoute: ActivatedRoute,
+              private location: Location) {
     this.currentDisplay = 'outs';
     this.panoMode = false;
     this.keyboard.add([
@@ -50,9 +54,16 @@ export class AisleViewComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.logoSubscription = this.logoService.logoClickEvent().subscribe(() => this.changePanoMode());
 
+    const missionId: number = Number(this.activatedRoute.params._value['missionId']);
+    const aisleId: number = Number(this.activatedRoute.params._value['aisleId']);
+
     this.apiService.getMissions().subscribe(missions => {
       this.missions = missions;
-      this.setMission(this.missions[0]);
+      missions.forEach(mission => {
+        if (mission.missionId === missionId) {
+          this.setMission(mission, aisleId);
+        }
+      });
     });
   }
 
@@ -68,11 +79,21 @@ export class AisleViewComponent implements OnInit, OnDestroy {
     this.resetPano = !this.resetPano;
   }
 
-  setMission(mission: Mission) {
+  changeMission(mission: Mission) {
+    this.apiService.getAisles(mission.missionId).subscribe(aisles => {
+      this.setMission(mission, aisles[0].aisleId);
+    });
+  }
+
+  setMission(mission: Mission, aisleId: number) {
     this.selectedMission = mission;
     this.apiService.getAisles(mission.missionId).subscribe(aisles => {
       this.aisles = aisles;
-      this.setAisle(this.aisles[0]);
+      aisles.forEach(aisle => {
+        if (aisle.aisleId === aisleId) {
+          this.setAisle(aisle);
+        }
+      });
     });
   }
 
@@ -85,6 +106,7 @@ export class AisleViewComponent implements OnInit, OnDestroy {
       this.currentDisplay = 'outs';
       this.currentId = null;
     });
+    this.location.replaceState('/mission/' + this.selectedMission.missionId + '/aisle/' + this.selectedAisle.aisleId);
   }
 
   setId(id: number) {
