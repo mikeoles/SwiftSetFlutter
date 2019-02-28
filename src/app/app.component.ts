1f88066d89ade7dfc0edb3848ebd98bdf464eded
Oles <moles@ceiamerica.com>
2019-02-28 13:22:26 -0500

    Add back buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { KeyboardShortcutsService } from 'ng-keyboard-shortcuts';
 import { LogoService } from './logo.service';
+import { BackService } from './back.service';
+import { faArrowAltCircleLeft } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,13 @@ import { LogoService } from './logo.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private router: Router, private logoService: LogoService) { }
+  displayBackButton: boolean;
+
+  faArrowAltCircleLeft = faArrowAltCircleLeft;
+
+  constructor(private router: Router, private logoService: LogoService, private backService: BackService) {
+    router.events.subscribe( (event) => ( event instanceof NavigationEnd ) && this.handleRouteChange() );
+  }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
@@ -25,4 +33,16 @@ export class AppComponent implements OnInit {
   logoClicked() {
     this.logoService.logoClick();
   }
+
+  backClicked() {
+    this.backService.backClick();
+  }
+
+  handleRouteChange = () => {
+    if (this.router.url.includes('store')) {
+      this.displayBackButton = false;
+    } else {
+      this.displayBackButton = true;
+    }
+  };
 }
