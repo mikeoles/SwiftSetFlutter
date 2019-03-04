9c3781eb6aaba476b278beab4c480323f4cb7474
Andy Miller <amcoder@gmail.com>
2019-03-04 15:56:42 -0500

    Fix invalid docker run script

--- docker-run.sh
+++ docker-run.sh
@@ -2,14 +2,15 @@ echo "Starting web app"
 
 echo "{" > /usr/share/nginx/html/assets/config.json
 if [[ ! -z "$DEMO_API_URL" ]]; then
-  echo "  \"apiUrl\": \"$DEMO_API_URL\"" >> /usr/share/nginx/html/assets/config.json
+  echo "  \"apiUrl\": \"$DEMO_API_URL\"," >> /usr/share/nginx/html/assets/config.json
 fi
 if [[ ! -z "$EXPORT_FIELDS" ]]; then
-  echo "  \"exportFields\": [$EXPORT_FIELDS]" >> /usr/share/nginx/html/assets/config.json
+  echo "  \"exportFields\": [$EXPORT_FIELDS]," >> /usr/share/nginx/html/assets/config.json
 fi
 if [[ ! -z "$PRODUCT_GRID_FIELDS" ]]; then
-  echo "  \"productGridFields\": [$PRODUCT_GRID_FIELDS]" >> /usr/share/nginx/html/assets/config.json
+  echo "  \"productGridFields\": [$PRODUCT_GRID_FIELDS]," >> /usr/share/nginx/html/assets/config.json
 fi
+echo "  \"production\": true" >> /usr/share/nginx/html/assets/config.json
 echo "}" >> /usr/share/nginx/html/assets/config.json
 
 echo "Using config:"
