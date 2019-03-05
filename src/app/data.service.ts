5daf5fb29706d05488dfa5493432f1411bfe9a12
Oles <moles@ceiamerica.com>
2019-03-05 14:32:23 -0500

    Pass average labels to mission view

---
+++ src/app/data.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+@Injectable()
+export class DataService {
+  averageStoreOuts: number;
+  averageStoreLabels: number;
+}
--- src/app/store-view/missions-grid/missions-grid.component.ts
+++ src/app/store-view/missions-grid/missions-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import MissionSummary from 'src/app/missionSummary.model';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-missions-grid',
@@ -10,14 +11,18 @@ import MissionSummary from 'src/app/missionSummary.model';
 export class MissionsGridComponent implements OnInit {
   @Input() missionSummaries: MissionSummary[];
   @Input() missionsDate: number;
+  @Input() averageStoreOuts: number;
+  @Input() averageStoreLabels: number;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public dataService: DataService) { }
 
   ngOnInit() {
   }
 
   viewMission(missionId: number) {
     this.router.navigate(['mission/' + missionId]);
+    this.dataService.averageStoreOuts = this.averageStoreOuts;
+    this.dataService.averageStoreLabels = this.averageStoreLabels;
   }
 
 }
