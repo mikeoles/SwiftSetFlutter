98b89075848f20fa135f80d929de59f2a45de6c8
Oles <moles@ceiamerica.com>
2019-04-02 10:14:59 -0400

    Only when username and password hash enabled

--- docker-run.sh
+++ docker-run.sh
@@ -20,11 +20,11 @@ echo "}" >> /usr/share/nginx/html/assets/config.json
 
 if [[ "$API_TYPE" = "static" ]]; then
   sed -i '/api.*/,+3d'  /etc/nginx/conf.d/default.conf
-  sed -i "/try_files/a\\\tauth_basic \"Restricted Content\";\n\tauth_basic_user_file /etc/nginx/conf.d/nginx_auth.htpasswd;" /etc/nginx/conf.d/default.conf
 fi
 
 if [[ ! -z "$USERNAME" ]] && [[ ! -z "$PASSWORD_HASH" ]]; then
   echo "$USERNAME:$PASSWORD_HASH" >> /etc/nginx/conf.d/nginx_auth.htpasswd
+  sed -i "/try_files/a\\\tauth_basic \"Restricted Content\";\n\tauth_basic_user_file /etc/nginx/conf.d/nginx_auth.htpasswd;" /etc/nginx/conf.d/default.conf
 fi
 
 echo "Using config:"
