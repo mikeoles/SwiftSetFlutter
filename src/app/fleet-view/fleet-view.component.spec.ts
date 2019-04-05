6f92b5a1882c52ce80165f56c11a2d3d0bd0dae5
Oles <moles@ceiamerica.com>
2019-04-05 15:16:13 -0400

    Css changes

--- src/app/fleet-view/fleet-view.component.spec.ts
+++ src/app/fleet-view/fleet-view.component.spec.ts
@@ -2,19 +2,50 @@
 
 // import { FleetViewComponent } from './fleet-view.component';
 // import { RouterTestingModule } from '@angular/router/testing';
+// import Store from '../store.model';
+// import DaySummary from '../daySummary.model';
+// import { ApiService } from '../api.service';
+// import { of } from 'rxjs';
 
 // describe('FleetViewComponent', () => {
 //   let component: FleetViewComponent;
 //   let fixture: ComponentFixture<FleetViewComponent>;
+//   let apiService: jasmine.SpyObj<ApiService>;
+
+//   const daySummaries: DaySummary[] = [
+//     {
+//       date: new Date('2018-12-12'),
+//       dailyAverage: 1,
+//     }
+//   ];
+//   const stores: Store[] = [{
+//     storeId: 1,
+//     storeName: '',
+//     storeAddress: '',
+//     totalAverageOuts: 1,
+//     totalAverageLabels: 1,
+//     totalAverageSpreads: 1,
+//     summaryOuts: daySummaries,
+//     summaryLabels: daySummaries,
+//     summarySpreads: daySummaries
+//   }];
 
 //   beforeEach(async(() => {
+//     const apiServiceSpy = jasmine.createSpyObj('ApiService', ['getStores']);
+
 //     TestBed.configureTestingModule({
 //       declarations: [ FleetViewComponent ],
 //       imports: [
 //         RouterTestingModule.withRoutes([])
 //       ],
+//       providers: [
+//         { provide: 'ApiService', useValue: apiServiceSpy },
+//       ]
 //     })
 //     .compileComponents();
+
+//     apiService = TestBed.get('ApiService');
+//     apiService.getStores.and.returnValue(of(stores));
 //   }));
 
 //   beforeEach(() => {
